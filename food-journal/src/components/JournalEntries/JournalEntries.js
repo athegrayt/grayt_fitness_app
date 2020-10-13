8b2c41a872bc7de2001896f4ddfcc58e47ee29ff
Conrad Wright <easp.conrad@gmail.com>
2020-10-12 18:06:24 -0600

    Code from the evening

--- food-journal/src/components/JournalEntries/JournalEntries.js
+++ food-journal/src/components/JournalEntries/JournalEntries.js
@@ -1,32 +1,25 @@
 import React from 'react'
 import JournalEntry from './JournalEntry/JournalEntry'
 import classes from './JournalEntries.module.css'
-	
-	
 
-const JournalEntries = (props) =>{
-   const journalEntryList = props.journalEntries
+const JournalEntries = (props) => {
+  console.log('props: ', props)
+  const { journalEntries } = props
 
-	   const entries = journalEntryList.map((entry, i) => {   
-		return (
-			<JournalEntry
-				key={entry.time}
-				id={i}
-				time={entry.time}
-				description={entry.description}
-				calories={entry.calories}
-				deleteHandler={()=>props.deleteHandler(i)}
-				deleteRequestHandler={()=>props.deleteRequestHandler(i)}
-				deleteRequest={entry.deleteRequest}
-			/>
-		);
-		   });
-	return(
-		<div className={classes.JournalEntries}>
-			{entries}
-		</div>
-	)
-   
-    
-};
-export default JournalEntries;
+  const entries = journalEntries.map((entry, i) => {
+    return (
+      <JournalEntry
+        key={entry.time}
+        id={i}
+        time={entry.time}
+        description={entry.description}
+        calories={entry.calories}
+        deleteHandler={() => props.deleteHandler(i)}
+        deleteRequestHandler={() => props.deleteRequestHandler(i)}
+        deleteRequest={entry.deleteRequest}
+      />
+    )
+  })
+  return <div className={classes.JournalEntries}>{entries}</div>
+}
+export default JournalEntries
--- food-journal/src/components/JournalEntries/JournalEntry/JournalEntry.js
+++ food-journal/src/components/JournalEntries/JournalEntry/JournalEntry.js
@@ -1,30 +1,50 @@
 import React from 'react'
 import classes from './JournalEntry.module.css'
 import PropTypes from 'prop-types'
-        
-const JournalEntry = (props) =>{
-   let deleteBtn = null
-	 props.deleteRequest ? deleteBtn = <button type="button" onClick={props.deleteHandler}>X</button> : deleteBtn = null;
 
-	return (
-			<div id={props.id} className={classes.JournalEntry} onClick={props.deleteRequestHandler}>
-				<p>{props.time}</p>
-				<p>{props.description}</p>
-				<p>cal: {props.calories}</p>
-				{deleteBtn}
-			</div>
-		);
-    
-};
+const JournalEntry = (props) => {
+  // const deleteRequestHandler = () => {
+  //   const updatedJournalEntry = this.state.journalEntries
+  //   if (updatedJournalEntry[entryID]) {
+  //     updatedJournalEntry[entryID].deleteRequest = !this.state.journalEntries[
+  //       entryID
+  //     ].deleteRequest
+  //     this.setState({
+  //       journalEntries: updatedJournalEntry,
+  //     })
+  //   }
+  // }
 
-JournalEntry.propTypes={
-	deleteRequest: PropTypes.bool,
-	deleteRequestHandler: PropTypes.func,
-	deleteHandler: PropTypes.func,
-	time: PropTypes.string,
-	description: PropTypes.string, 
-	calories: PropTypes.number,
-	id: PropTypes.number
+  let deleteBtn = null
+  props.deleteRequest
+    ? (deleteBtn = (
+        <button type='button' onClick={props.deleteHandler}>
+          X
+        </button>
+      ))
+    : (deleteBtn = null)
+
+  return (
+    <div
+      id={props.id}
+      className={classes.JournalEntry}
+      onClick={props.deleteRequestHandler}>
+      <p>{props.time}</p>
+      <p>{props.description}</p>
+      <p>cal: {props.calories}</p>
+      {deleteBtn}
+    </div>
+  )
 }
 
-export default JournalEntry;
+JournalEntry.propTypes = {
+  deleteRequest: PropTypes.bool,
+  deleteRequestHandler: PropTypes.func,
+  deleteHandler: PropTypes.func,
+  time: PropTypes.string,
+  description: PropTypes.string,
+  calories: PropTypes.number,
+  id: PropTypes.number,
+}
+
+export default JournalEntry
