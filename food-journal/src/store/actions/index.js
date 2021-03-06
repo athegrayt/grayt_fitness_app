5d07a94090179afdf43105756746b2afcaf9fb8e
Akil Gray <akilgray@Akils-MacBook-Air.local>
2021-03-06 11:50:35 -0500

    Readme file updated

--- food-journal/src/store/actions/index.js
+++
@@ -1,9 +0,0 @@
-export { entryDelete, initEntries, dbUpdate } from "./journalEntries";
-export { addEntry } from "./foodSearch";
-export { auth, logout, authCheckState } from "./auth";
-export {
-  setCalGoal,
-  setInfo,
-  fetchInfo,
-  setAuthRedirectPath,
-} from "./userInfo";
